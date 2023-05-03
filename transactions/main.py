13043adb29cda89e23775cf4787009e350cc033b
airat <c098vm@ya.ru>
2023-05-04 00:22:33 +0500

    Class funcs added

--- transactions/main.py
+++ transactions/main.py
@@ -1,15 +1,6 @@
 import utils
 
 PATH = "data/operations.json"
-
 data = utils.load_transactions(PATH)
 
-# n = 100
-# date_data = d[n]['date']
-# from_data = d[n]['from']
-# to_data = d[n]['to']
-# amount_data = d[n]['operationAmount']['amount']
-# currency_name_data = d[n]['operationAmount']['currency']['name']
-# print(utils.convert_date(date_data), d[n]['description'])
-# print(f"{utils.card_account_choice(from_data)} -> {utils.card_account_choice(to_data)}")
-# print (amount_data, currency_name_data)
+
